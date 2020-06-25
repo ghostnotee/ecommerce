9ee95bff5b1f50a5b50fcf1736f2bd4c571df1ec
Selçuk Bilgen <selcukbilgen@hotmail.com>
2020-06-26 02:27:59 +0300

    template prepare

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,24 +1,25 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="{{config('app.locale')}}">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title',config('app.name'))</title>
+    @include('layouts.partials.head')
+    @yield('head')
 </head>
-<body>
 
-{{------TopNavBar------}}
+<body id="commerce">
+{{------TopNavBar --}}
 @include('layouts.partials.navbar')
 
 {{------Content------}}
 @yield('content')
-<hr>
-Laravel Eticaret
 
 {{------Footer------}}
-@include('layouts.partials.footer', ['year'=> date('Y')])
+@include('layouts.partials.footer')
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+@yield('footer')
 </body>
+
 </html>
