7b88d6e858f74b00748d358c39100c44ce2f01cd
Selçuk Bilgen <selcukbilgen@hotmail.com>
2020-06-26 15:31:19 +0300

    payment page created

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ Route::get('/', 'HomePageController@index')->name('homepage');
 Route::get('/category/{slug_categoryname}', 'CategoryController@index')->name('category');
 Route::get('/product/{slug_productname}', 'ProductController@index')->name('product');
 Route::get('/shoppingcart', 'ShoppingCartController@index')->name('shoppingcart');
+Route::get('/payment','PaymentController@index')->name('payment');
