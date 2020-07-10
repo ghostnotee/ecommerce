7b6a6831d38524d22b7431abfdaf3e8eac7b5f4c
Selçuk Bilgen <selcukbilgen@hotmail.com>
2020-07-11 01:06:19 +0300

    search feature added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,4 +13,15 @@ class ProductController extends Controller
         $categories = $product->categories()->distinct()->get();
         return view('product', compact('product', 'categories'));
     }
+
+    public function search(Request $request)
+    {
+        $lookFor = request()->input('search');
+        $products = Product::where('product_name', 'like', "%$lookFor%")
+            ->orWhere('description', 'like', "%$lookFor%")
+            ->get();
+
+        $request->flash();
+        return view('searchresult', compact('products'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'HomePageController@index')->name('homepage');
 Route::get('/category/{slug_categoryname}', 'CategoryController@index')->name('category');
 Route::get('/product/{slug_productname}', 'ProductController@index')->name('product');
+Route::post('/search', 'ProductController@search')->name('product_search');
 Route::get('/shoppingcart', 'ShoppingCartController@index')->name('shoppingcart');
 Route::get('/payment', 'PaymentController@index')->name('payment');
 Route::get('/orders', 'OrderController@index')->name('orders');
 Route::get('/orderdetails/{id}', 'OrderController@orderDetails')->name('orderdateils');
 
-Route::group(['prefix' => '/user'],function (){
+Route::group(['prefix' => '/user'], function () {
     Route::get('/signin', 'UserController@signInForm')->name('user.signin');
     Route::get('/register', 'UserController@registerForm')->name('user.register');
 });
