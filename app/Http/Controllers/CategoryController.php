8bf4a7cce5c467111378cf297c480a1d3fbb6be2
Selçuk Bilgen <selcukbilgen@hotmail.com>
2020-07-12 19:48:07 +0300

    sorting of products

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -12,7 +12,22 @@ class CategoryController extends Controller
         $category = Category::where('slug', $slugCategoryName)->firstOrFail();
         $subCategories = Category::where('up_id', $category->id)->get();
 
-        $products = $category->products()->paginate(3);
+        $order = request('order');
+
+        switch ($order) {
+            case 'mostselling':
+                $products = $category->products()
+                    ->distinct()
+                    ->join('product_details', 'product_details.product_id', 'products.id')
+                    ->latest('updated_at')
+                    ->paginate(2);
+
+            case 'newproduct':
+                $products = $category->products()->distinct()->orderByDesc('updated_at')->paginate(2);
+
+            default :
+                $products = $category->products()->distinct()->paginate(3);
+        }
 
         return view('category', compact('category', 'subCategories', 'products'));
     }
