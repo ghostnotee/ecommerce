bd52942b689f233d596fe6098b3ac13c73175c0e
Selçuk Bilgen <selcukbilgen@hotmail.com>
2020-07-18 02:19:46 +0300

    Products show in cart page

--- resources/views/shoppingcart.blade.php
+++ resources/views/shoppingcart.blade.php
@@ -6,42 +6,59 @@
             <h2>Sepet</h2>
             @include('layouts.partials.alert')
 
-            <table class="table table-bordererd table-hover">
-                <tr>
-                    <th>Ürün</th>
-                    <th>Tutar</th>
-                    <th>Adet</th>
-                    <th>Ara Toplam</th>
-                    <th>İşlem</th>
-                </tr>
-                <tr>
-                    <td colspan="5">Henüz sepette ürün yok</td>
-                </tr>
-                <tr>
-                    <td><img src="http://lorempixel.com/120/100/food/2"> Ürün adı</td>
-                    <td>18.99</td>
-                    <td>
-                        <a href="#" class="btn btn-xs btn-default">-</a>
-                        <span style="padding: 10px 20px">1</span>
-                        <a href="#" class="btn btn-xs btn-default">+</a>
-                    </td>
-                    <td>18.99</td>
-                    <td>
-                        <a href="#">Sil</a>
-                    </td>
-                </tr>
-                <tr>
-                    <th></th>
-                    <th></th>
-                    <th>Toplam Tutar (KDV Dahil)</th>
-                    <th>18.99</th>
-                    <th></th>
-                </tr>
-            </table>
-            <div>
-                <a href="#" class="btn btn-info pull-left">Sepeti Boşalt</a>
-                <a href="#" class="btn btn-success pull-right btn-lg">Ödeme Yap</a>
-            </div>
+            @if(count(Cart::content())>0)
+                <table class="table table-bordererd table-hover">
+                    <tr>
+                        <th colspan="2">Ürün</th>
+                        <th>Adet Fiyatı</th>
+                        <th>Adet</th>
+                        <th>Tutar</th>
+                    </tr>
+                    @foreach(Cart::content() as $productCartItem)
+                        <tr>
+                            <td style="width: 120px;">
+                                <a href="{{route('product',$productCartItem->options->slug)}}">
+                                    <img src="http://via.placeholder.com/120x100?text=ÜrünResmi">
+                                </a>
+                            </td>
+                            <td>
+                                <a href="{{route('product',$productCartItem->options->slug)}}">
+                                    {{$productCartItem->name}}</a>
+                            </td>
+                            <td>{{$productCartItem->price}} ₺</td>
+                            <td>
+                                <a href="#" class="btn btn-xs btn-default">-</a>
+                                <span style="padding: 10px 20px">{{$productCartItem->qty}}</span>
+                                <a href="#" class="btn btn-xs btn-default">+</a>
+                            </td>
+                            <td>{{$productCartItem->subtotal }} ₺</td>
+                            <td class="text-right">
+                                <a href="#">Sil</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                    <tr>
+                        <th colspan="4" class="text-right">Alt Toplam</th>
+                        <th class="text-right">{{Cart::subtotal()}} ₺</th>
+                    </tr>
+                    <tr>
+                        <th colspan="4" class="text-right">KDV</th>
+                        <th class="text-right">{{Cart::tax()}} ₺</th>
+                    </tr>
+                    <tr>
+                        <th colspan="4" class="text-right">Genel Toplam</th>
+                        <th class="text-right">{{Cart::total()}} ₺</th>
+                    </tr>
+                </table>
+                <div>
+                    <a href="#" class="btn btn-info pull-left">Sepeti Boşalt</a>
+                    <a href="#" class="btn btn-success pull-right btn-lg">Ödeme Yap</a>
+                </div>
+            @else
+                <p>Sepetinizde ürün yok!</p>
+            @endif
+
+
         </div>
     </div>
 @endsection
