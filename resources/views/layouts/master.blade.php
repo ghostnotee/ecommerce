34ac98dee1d3f2b946fd8e468bfbb2a86d778379
Selçuk Bilgen <selcukbilgen@hotmail.com>
2020-07-31 00:03:34 +0300

    admin template

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,17 +8,18 @@
 </head>
 
 <body id="commerce">
-{{------TopNavBar --}}
+{{------    TopNavBar   ------}}
 @include('layouts.partials.navbar')
 
-{{------Content------}}
+{{------    Content ------}}
 @yield('content')
 
-{{------Footer------}}
+{{------    Footer  ------}}
 @include('layouts.partials.footer')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="/js/app.js"></script>
 @yield('footer')
 </body>
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+/*mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');*/
+
+mix.copy('resources/js/admin-app.js', 'public/js/admin-app.js');
+mix.sass('resources/sass/admin.scss', 'public/css');
+mix.sass('resources/sass/login.scss', 'public/css');
