7391f501660a8f3711bb4a325e766a75875ec1f1
Selçuk Bilgen <selcukbilgen@hotmail.com>
2020-08-05 22:40:03 +0300

    admin authenticate

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,11 +4,33 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function signInForm()
+    public function signInForm(Request $request)
     {
+        if ($request->isMethod('post')) {
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+
+            if (Auth::guard('admin')->attempt([
+                'email' => $request->email, 'password' => $request->password, 'is_admin' => 1], true)) {
+
+                return redirect()->route('admin.homepage');
+            } else {
+                return back()->withInput()->withErrors(['email' => 'Giriş Hatalı !']);
+            }
+        }
+
         return view('admin.signin');
+    }
+
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin.signin');
     }
 }
