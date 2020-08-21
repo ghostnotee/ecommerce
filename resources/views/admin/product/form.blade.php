0180899dfea87521c92ea0d3ac733eb6c9e7f8a0
Selçuk Bilgen <selcukbilgen@hotmail.com>
2020-08-21 13:53:55 +0300

    using the laravel filemanager

--- resources/views/admin/product/form.blade.php
+++ resources/views/admin/product/form.blade.php
@@ -116,6 +116,7 @@
 @endsection
 @section('footer')
     <script src="//cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ckeditor/4.14.1/plugins/autogrow/plugin.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
     <script>
         $(function () {
@@ -124,8 +125,18 @@
             });
 
             var options = {
-                uiColor: '#f4645f',
-                language: 'tr'
+                uiColor: '#a76c6c',
+                language: 'tr',
+                extraPlugins: 'autogrow',
+                autoGrow_minHeight: 250,
+                autoGrow_maxHeight: 600,
+                filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
+                filebrowserImageUploadUrl:
+                    '/laravel-filemanager/upload?type=Images&_token=',
+                filebrowserBrowseUrl:
+                    '/laravel-filemanager?type=Files',
+                filebrowserUploadUrl:
+                    '/laravel-filemanager/upload?type=Files&_token='
             }
             CKEDITOR.replace('description', options);
         });
