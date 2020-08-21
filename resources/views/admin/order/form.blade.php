27d818350240a9df2e33a4eea5645e8930c1d1fe
Selçuk Bilgen <selcukbilgen@hotmail.com>
2020-08-22 00:59:31 +0300

    listing products on product details

--- resources/views/admin/order/form.blade.php
+++ resources/views/admin/order/form.blade.php
@@ -73,6 +73,44 @@
                 </div>
             </div>
         </div>
+
+        <h2>Sipariş (SP-{{$order->id}})</h2>
+        <table class="table table-bordered table-hover">
+            <tr>
+                <th colspan="2">Ürün</th>
+                <th>Tutar</th>
+                <th>Adet</th>
+                <th>Ara Toplam</th>
+                <th>Durum</th>
+            </tr>
+            @foreach($order->shoppingcart->shoppingcartProducts as $shoppingcartProduct)
+                <tr>
+                    <td style="width: 120px">
+                        <a href="{{ route('product', $shoppingcartProduct->product->slug) }}">
+                            <img src=" {{ $shoppingcartProduct->product->details->product_photo != null ? asset('uploads/products/'.$shoppingcartProduct->product->details->product_photo) :
+                                'http://via.placeholder.com/120x100?text=Ürün_Fotoğrafı'}} " style="height:120px">
+                        </a>
+                    </td>
+                    <td>{{$shoppingcartProduct->product->product_name }}</td>
+                    <td>{{$shoppingcartProduct->price}} ₺</td>
+                    <td>{{$shoppingcartProduct->quantity}}</td>
+                    <td>{{$shoppingcartProduct->price * $shoppingcartProduct->quantity}} ₺</td>
+                    <td>{{$shoppingcartProduct->status}}</td>
+                </tr>
+            @endforeach
+            <tr>
+                <th colspan="4" class="text-right">Toplam Tutar</th>
+                <td colspan="2">{{$order->order_amount}} ₺</td>
+            </tr>
+            <tr>
+                <th colspan="4" class="text-right">Toplam Tutar (KDV'li)</th>
+                <td colspan="2">{{$order->order_amount * ((100+config('cart.tax'))/100)}} ₺</td>
+            </tr>
+            <tr>
+                <th colspan="4" class="text-right">Sipariş Durumu</th>
+                <td colspan="2">{{$order->status}}</td>
+            </tr>
+        </table>
         <div class="row">
             <div class="col-sm-2">
                 <button type="submit" class="btn btn-primary">
